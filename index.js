bafd8f204072f44797edc848f8b4d9a8dd1a1032
Lara Grocke <laragrocke@Laras-MacBook-Air.local>
2022-11-16 23:54:14 +1030

    Minor HTML Change

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const fs = require('fs');
 const inquirer = require('inquirer');
-const path = require('path');
 
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
